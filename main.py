a689eccc4d52fe7898aabab570ebef3bbce7f36a
YJoe <YJoe762@gmail.com>
2017-11-17 15:03:07 +0000

    Loading and showing an image

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import numpy
 import cv2
 
-print("Yay")
+cv2.namedWindow("a duck", cv2.WINDOW_NORMAL)
+duck_image = cv2.imread("C:\duck-05.jpg")
+cv2.imshow("a duck", duck_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
