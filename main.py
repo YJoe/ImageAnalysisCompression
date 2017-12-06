7280c816794e5bd93d38815afacffa5b769e9561
YJoe <YJoe762@gmail.com>
2017-12-06 13:29:34 +0000

    Writing and reading binary files

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def append_to_node_tree(root, node_values):
             #print "\t\t\t\tThe next node frequency [", node_values[0].frequency, "] is >= [", root.frequency, "]"
             left_node = node_values[0]
             del node_values[0]
-            root = Node(left_node, root, str(left_node.value + root.value), left_node.frequency + root.frequency)
+            root = Node(left_node, root, str(left_node.value) + str(root.value), left_node.frequency + root.frequency)
             #print "\t\t\t\t\tAppended left node [", left_node.value, "] to existing tree"
             return append_to_node_tree(root, node_values)
 
@@ -87,9 +87,9 @@ def append_to_node_tree(root, node_values):
     return root, [] if node_values is None else node_values
 
 
-def get_block_string(block):
+def get_block_channel_values(block):
 
-    string = ""
+    channel_values = []
 
     # get the size of the block (should be 8)
     n = len(block)
@@ -103,7 +103,8 @@ def get_block_string(block):
         x = j
         y = k
         while x >= 0 and y < len(block[0]):
-            string += str(block[x][y]) + " "
+            for l in range(0, 3):
+                channel_values.append(block[x][y][l])
             x -= 1
             y += 1
 
@@ -115,20 +116,7 @@ def get_block_string(block):
         else:
             k += 1
 
-    return string
-
-
-def huffman_string_block(block):
-
-    # split the block into three channels
-    block_channels = cv2.split(block)
-
-    huffman_channels = ["", "", ""]
-
-    for i in range(0, len(block_channels)):
-        huffman_channels[i] = get_block_string(block_channels[i])
-
-    return huffman_channels
+    return channel_values
 
 
 def create_tree(node_values):
@@ -154,7 +142,7 @@ def create_tree(node_values):
             # start a tree
             left = node_values[1]
             right = node_values[0]
-            root = Node(left, right, str(left.value + "" + right.value), left.frequency + right.frequency)
+            root = Node(left, right, str(left.value) + str(right.value), left.frequency + right.frequency)
 
             # remove the two nodes that are now in the tree
             del node_values[0]
@@ -419,52 +407,136 @@ def decompress_block(block, quantization_table):
     return block
 
 
-def write_to_bin(file_name, byte_string):
+def write_to_bin(file_name, frequency_list, quality_factor, image_width, image_height, border_w, border_h, bin_str):
 
-    print "Writing bin string"
+    # create the header for the binary file holding the width and height followed by the frequency list
+    header_str = str(int(quality_factor)) + "_" + str(image_width) + "_" + str(image_height) + "_" + str(border_w) + "_" + str(border_h) + "_"
+    print "Writing header string [", header_str, "]"
+    for i in range(0, len(frequency_list)):
+        header_str += str(frequency_list[i][0]) + "_" + str(frequency_list[i][1]) + "_"
+    header_str += "\n"
+
+    # work out how many bits will wasted by slicing into bytes
+    bit_waste = (8 - (len(bin_str) % 8)) % 8
+
+    # store bitwaste into the first byte of the code so that we know how much to ignore when decoding
+    byte_chunks = ["{0:b}".format(bit_waste).zfill(8)]
 
     # cut the string into groups of 8 char making sure that any left over are prefixed by 0
-    byte_chunks = [byte_string[i:i+8].zfill(8) for i in range(0, len(byte_string), 8)]
-
-    bytes = []
+    byte_chunks += [bin_str[i:i+8].zfill(8) for i in range(0, len(bin_str), 8)]
 
     # convert bytes into numbers
+    byte_arr = []
     for b in byte_chunks:
-        bytes.append(int(b, 2))
+        byte_arr.append(int(b, 2))
 
-    byte_arr = bytearray(bytes)
+    # convert the numbers into actual bytes
+    byte_array = bytearray(byte_arr)
 
-    print byte_arr
+    # the first line of the file will just be ascii for the width and height of the image
     bin_file = open(file_name, "wb")
-    bin_file.write(bytes(byte_arr))
+    bin_file.write(header_str)
+    bin_file.write(byte_array)
 
 
 def read_from_bin(file_name):
-    print "Reading from bin"
+
+    # read the file by appending bytes to array
     f = open(file_name, "rb")
     file_bytes = []
-
+    header = ""
+    done_with_header = False
     try:
-        byte = f.read(1)
-        while byte != "":
-            file_bytes.append(byte)
-            byte = f.read(1)
+        b = f.read(1)
+        while b != "":
+            if not done_with_header:
+                if b == "\n":
+                    done_with_header = True
+                    b = f.read(1)
+                else:
+                    header += b
+                    b = f.read(1)
+            else:
+                file_bytes.append(b)
+                b = f.read(1)
     finally:
         f.close()
 
-    for file_byte in file_bytes:
-        process_byte(file_byte)
+    print len(file_bytes)
+
+    # reform the string from all bytes
+    bit_string = "".join("{0:b}".format(ord(file_byte)).zfill(8) for file_byte in file_bytes)
+
+    # remove any of the wasted bits that we dont want to be considered part of the huffman code
+    bit_waste = "{0:b}".format(ord(file_bytes[0])).zfill(8)
+    last_byte = bit_string[len(bit_string) - 8: len(bit_string)]
+    last_byte = last_byte[int(bit_waste, 2): len(last_byte)]
+
+    # remove the bit waste header
+    bit_string = bit_string[8 : len(bit_string) - 8] + last_byte
+
+    return header, bit_string
 
 
-# DCT
-if __name__ == "__main__":
+def rearrange_block(block_1d, block_size):
 
-    #img = cv2.imread('images/turtle.jpg', 1)
-    img = cv2.imread('images/fish.jpg', 1)
+    # create a 3d array with the size 8 * 8 * 3
+    block = []
+    for i in range(0, block_size):
+        temp_line = []
+        for j in range(0, block_size):
+            temp_channel = []
+            for k in range(0, 3):
+                temp_channel.append(0)
+            temp_line.append(temp_channel)
+        block.append(temp_line)
 
-    cv2.imshow("ORIGINAL", img)
-    cv2.waitKey()
+    # get the size of the block (should be 8)
+    n = len(block)
+    j = 0
+    k = 0
+    c = 0
 
+    # for all of the left and bottom edge indexes for this n
+    for i in range(0, n * 2 - 1):
+
+        # for the current edge loop through the diagonal from this point
+        x = j
+        y = k
+        while x >= 0 and y < len(block[0]):
+            for l in range(0, 3):
+                block[x][y][l] = block_1d[c]
+                c += 1
+            x -= 1
+            y += 1
+
+        # if i is still less than the block height keep iterating up
+        if i < n - 1:
+            j += 1
+
+        # i reached the mid point, start moving the other way
+        else:
+            k += 1
+
+    return block
+
+
+def array_to_image(arr):
+
+    # the size of the block
+    n = len(arr)
+
+    blank_image = np.zeros((n, n, 3), np.int8)
+
+    for i in range(0, n):
+        for j in range(0, n):
+            for k in range(0, 3):
+                blank_image[i][j][k] = arr[i][j][k]
+
+    return blank_image
+
+
+def compress_image(img, quality_factor, file_name):
     # define the size of a block
     dct_width = 8
     dct_height = 8
@@ -479,42 +551,126 @@ if __name__ == "__main__":
     image_height = img.shape[1]
 
     # define the quality to compress, 0 is low quality 99 is high, then get quanization tables
-    quality_factor = 10.0
+    print "Getting quantisation tables for quality [", quality_factor, "]"
     Q = get_quantisation_channels(quality_factor)
 
     # convert from BGR to YCrBr colour space
+    print "Converting to YCrCb"
     image_ycc = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
 
     # divide the image into several 8*8*3 images
+    print "Splitting image into 8 * 8 * 3 blocks"
     image_blocks = split_blocks(image_ycc, dct_width, dct_height)
 
     # compress blocks using the quantisation tables made eariler
+    print "Compressing blocks with DCT"
     compressed_blocks = []
     for i in range(0, len(image_blocks)):
         compressed_blocks.append(compress_block(image_blocks[i], Q))
 
+    # store all block values
+    print "Getting all block values"
+    block_channel_values = []
     for i in range(0, len(image_blocks)):
-        h = huffman_string_block(compressed_blocks[i])
-        print ""
-        for i in h:
-            print i
+        block_channel_values.append(get_block_channel_values(compressed_blocks[i]))
+
+    # join all values into a string for tree analysis
+    print "Joining values to str"
+    all_block_channel_values_str = " ".join(str(v) for b in block_channel_values for v in b)
+
+    # get the frequency of each of the value so that we can build a tree
+    print "Creating huffman tree"
+    frequency_list = get_frequency_list(all_block_channel_values_str)
+
+    node_list = create_nodes(frequency_list)
+    root_node = create_tree(node_list)[0]
+
+    # encode the big string we made
+    print "Encoding str with huffman tree"
+    bin_str = encode(root_node, all_block_channel_values_str)
+
+    # make a bit of a guess as to how big the file will be
+    print "Current string size   [", len(bin_str) * 8, "(bits), ", len(bin_str), "(bytes), ", len(bin_str) / 1024, "(kb)]"
+    print "Estimate on file size [", len(bin_str), "(bits), ", len(bin_str) / 8, "(bytes), ", (len(bin_str) / 8) / 1024, "(kb)]"
+
+    # write the binary string to a file
+    print "Writing encoded image to bin"
+    write_to_bin(file_name, frequency_list, quality_factor, image_width, image_height, border_w, border_h, bin_str)
+
+
+def decompress_image(file_name):
+
+    dct_width = 8
+    dct_height = 8
+
+    # read in the file header and body
+    header, bin_str = read_from_bin(file_name)
+    print header
+
+    # split the header into segments the first three being the quality factor, width and height
+    h = header.split("_")
+    quality_factor = int(h[0])
+    image_width = int(h[1])
+    image_height = int(h[2])
+    border_w = int(h[3])
+    border_h = int(h[4])
+
+    print "Quality factor [", quality_factor, "]"
+    print "Image width [", image_width, "]"
+    print "Image height [", image_height, "]"
+    print "Border width [", border_w, "]"
+    print "Border height [", border_h, "]"
+
+    Q = get_quantisation_channels(quality_factor)
+
+    frequency_list = []
+    for i in range(5, len(h) - 1, 2):
+        frequency_list.append([h[i], int(h[i+1])])
+
+    node_list = create_nodes(frequency_list)
+    root_node = create_tree(node_list)[0]
+
+    # get a directory of all of the values for faster comparisons
+    print "Getting directory from root node"
+    decode_dir = get_decode_directory(root_node)
+
+    # decode the binary string
+    print "Using directory to decode binary string"
+    decoded_image_string = directory_decode(root_node, decode_dir, bin_str)
+
+    # split the decoded string into a string for each block
+    decoded_image_arr = decoded_image_string.split(" ")
+    decoded_blocks = []
+    for i in range(0, len(decoded_image_arr), 192):
+        decoded_blocks.append(array_to_image(rearrange_block(decoded_image_arr[i : i + 192], dct_width)))
 
     # decompress blocks using the same quantisation table
+    print "Decompressing blocks with IDCT"
     decompressed_blocks = []
-    for i in range(0, len(compressed_blocks)):
-        decompressed_blocks.append(decompress_block(compressed_blocks[i], Q))
+    for i in range(0, len(decoded_blocks)):
+        decompressed_blocks.append(decompress_block(decoded_blocks[i], Q))
 
-    # stitch together all of the compressed blocks
+    # stitch together all of the decompressed blocks
+    print "Stitching together all 8 * 8 * 3 blocks"
     image_ycc = combine_blocks(decompressed_blocks, image_width, image_height, dct_width, dct_height)
 
     # convert the new compressed image back into the BGR colour space
+    print "Converting to BGR"
     image = cv2.cvtColor(image_ycc, cv2.COLOR_YCrCb2BGR)
 
     # trim off the border we crated earlier
+    print "Removing any extra border"
     image = image[0: image_width - border_h, 0: image_height - border_w]
 
     cv2.imshow("COMPRESSED", image)
     cv2.waitKey()
+
+
+
+if __name__ == "__main__":
+
+    #compress_image(cv2.imread('images/turtle.ppm', 1), 10.0, "test.bin")
+    decompress_image("test.bin")
 
 
 # HUF
